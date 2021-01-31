2155cb4335f2e5123dd237f197be41bd0983c773
devtack <cance0311@gmail.com>
2021-01-31 15:21:32 +0900

    a

--- api.ts
+++ api.ts
@@ -1,31 +1,27 @@
-import axios from "axios";
+// import axios from "axios";
 
-const API_KEY = "비밀번호";
+// const API_KEY = "비밀번호";
 
-const makeRequest = (path: String, params: {}) =>
-  axios.get(`https://localhost.3000/${path}`, {
-    params: {
-      ...params,
-      api_key: API_KEY,
-    },
-  });
+// export const makeRequest = (path: String, params: {}) =>
+//   axios.get(`https://localhost.3000/${path}`, {
+//     params: {
+//       ...params,
+//     },
+//   });
 
-const getAnything = async (path: String, params = {}) => {
-  try {
-    const {
-      data: { results },
-      data,
-    } = await makeRequest(path, params);
-    return [results || data, null];
-  } catch (e) {
-    console.log(e);
-    return [null, e];
-  }
-};
+// export const getAnything = async (path: String, params = {}) => {
+//   try {
+//     const data = await makeRequest(path, params);
+//     return [data];
+//   } catch (e) {
+//     console.log(e);
+//     return [null, e];
+//   }
+// };
 
-export const myApi = {
-  getHome: () => getAnything("/api/home"),
-  getHotpost: () => getAnything("/api/hotpost"),
-  search: (query: string) => getAnything("/search/", { query }),
-  user: (id: string) => getAnything(`/user/findone/${id}`),
-};
+// export const myApi = {
+//   getHome: () => getAnything("api/stats"),
+//   getHotpost: () => getAnything("api/hotpost"),
+//   search: (query: string) => getAnything("search/", { query }),
+//   user: (id: string) => getAnything(`user/findone/${id}`),
+// };
--- screens/Home/HomeScreen.tsx
+++ screens/Home/HomeScreen.tsx
@@ -1,8 +1,10 @@
-import * as React from 'react';
+import React, { useEffect, useState } from "react";
 import { StyleSheet} from 'react-native';
 import { Text, View } from '../../components/Themed';
 import styled from 'styled-components/native';
 import { useNavigation } from '@react-navigation/native';
+// import { makeRequest, getAnything, myApi } from '../../api';
+import axios from 'axios';
 
 const Container = styled.View`
 background-color:#2B89C6;
@@ -13,10 +15,15 @@ const Touch = styled.TouchableOpacity`
 background-color:#fff;
 `;
 
-export default function HomeScreen() {
+
+export default function HomeScreen()  {
+  const c = async ()=> await axios.get(`https://localhost.3000/api.stats`)
+  c();
+  console.log(c())
   const navigation = useNavigation();
   const move = ()=>{
-  navigation.navigate('Com1Screen')
+  navigation.navigate('Com1Screen');
+  
   }
   return (
         <Container>
